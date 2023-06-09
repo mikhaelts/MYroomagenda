29aa71829a2d9641531bc3e25203576ff93572d8
MIKHAEL TELLYS SANTOS DE SANTANA <88353665+mikhaelts@users.noreply.github.com>
2023-06-09 14:32:20 -0300

    Delete myrrom directory

--- myrrom/src/main/java/com/myroom/myrrom/dataBase/RepositorioCadastro.java
+++
@@ -1,9 +0,0 @@
-package com.myroom.myrrom.dataBase;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.myroom.myrrom.entidades.Cadastro;
-
-public interface RepositorioCadastro extends JpaRepository<Cadastro,Long> {
-    
-}
